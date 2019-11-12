0c28a95c47be6fa03ac9be0526088b6b12f51d85
saideep-contentstack <57660981+saideep-contentstack@users.noreply.github.com>
2019-11-12 18:37:35 +0530

    Update validContentType.js

--- src/Middleware/validContentType.js
+++ src/Middleware/validContentType.js
@@ -8,13 +8,11 @@ let validContentType=()=>{return new Promise((resolve,reject)=>{
         .then((response)=>{
             response.data.content_types.forEach((e)=>{
                 if(e.uid === content_type){
-                    console.log("from valid contenttype middleware")
                     resolve(true)
                 }
         })
         reject(false)
         }).catch((error)=>{
-            console.log("from invalid contenttype middleware")
             reject(false);
         })
     }catch{
