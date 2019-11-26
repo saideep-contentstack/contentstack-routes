3e1b138d7c7d4d5fcd1054cfc8764faff9c76dcd
saideep-ss <57660981+saideep-ss@users.noreply.github.com>
2019-11-26 15:59:45 +0530

    Update Assets.js

--- src/Component/Assets/Assets.js
+++ src/Component/Assets/Assets.js
@@ -1,99 +1,67 @@
 /* eslint-disable no-undef */
-import React, {Fragment} from 'react';
-import {Redirect,Switch} from 'react-router-dom';
+import React, {useState,useEffect} from 'react';
+import {Switch,useParams} from 'react-router-dom';
 import RouteWithSubRoutes from '../RouteWithSubRoutes/RouteWithSubRoutes';
 import LoadingComponent from '../LoadingComponent/LoadingComponent';
+import Axios from 'axios';
 
-class Assets extends React.Component{
-    constructor(props){
-        super(props)
-        this.state={
-            r:<LoadingComponent/>,
-            isauth:false,
-            render:<LoadingComponent/>
+export default function Assets(props){
+  const parameter=useParams();
+  const [render,setRender]=useState(<tr><LoadingComponent/></tr>);
+  useEffect(()=>{
+    const stackKey=parameter.id;
+    const getAssets= async()=>{
+      let render=[];
+      let result =await Axios.get('/v3/assets',
+      {
+        headers:{
+          "api_key":stackKey
         }
-        this.logoutHandler=this.logoutHandler.bind(this);
-    }
-    async logoutHandler () {
-        let result = await axios.delete ('/user-session');
-        console.log (result);
-        this.setState ({isauth: false, render: <Redirect to="/login" />});
-      }
-    async componentDidMount(){
-       try{
-        let r=[]
-        let url=window.location.pathname;
-        let stackIdArray=url.split('/')
-        let stackId=stackIdArray[2];
-        const callApi=async ()=>{
-            let result=await axios.get('/v3/assets',
-                                    {headers:{
-                                        "api_key":stackId
-                                    }});
-            result.data.assets.forEach((e,i)=>{
-                r.push(<tr key={i}>
-                    <td>{e.title}</td>
-                    <td><img width="200px" height="200px" src={e.url} alt={e.title}/></td>
-                    <td>{e.updated_at}</td>
-                </tr>)
-            })
-            this.setState({r:r})
-        }
-        try{
-        callApi()
-        }catch{
-            console.log("Error at api")
-        }
-           this.setState({isauth:true})
-       }catch{
-           console.log("from catch");
-       }
-           this.setState({isauth:true})
-   }
+      });
+    result.data.assets.forEach((e,i)=>{
+      render.push(
+        <tr key={i}>
+          <td>{e.title}</td>
+          <td><img width="200px" height="200px" src={e.url} alt={e.title}/></td>
+          <td>{e.updated_at}</td>
+      </tr>
+      )
+    })
+    setRender(render)
+  }
+  getAssets();
+  },[])
 
-render(){
-   let t=this.state.render
-   if(this.state.isauth){
-    return(
-        <React.Fragment>
-        <div>
-           <div className="message-display">Assets goes here</div>
-           <br />
-               <table>
-                   <thead>
-                       <tr>
-                        <td>Title</td>
-                        <td>Image</td>
-                        <td>Updated at</td>
-                       </tr>
-                   </thead>
-                   <tbody>
-                       <tr>
-                           <td></td>
-                           <td></td>
-                           <td></td>
-                       </tr>
-                   {this.state.r}
-                   </tbody>
-                   <tfoot></tfoot>
-               </table>
-               </div>{this.props.routes?
+  return(
+    <>
+      <div>
+        <div className="message-display">Assets goes here</div>
+        <br/>
+        <table>
+          <thead>
+            <tr>
+              <td>Title</td>
+              <td>Image</td>
+              <td>Updated at</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+              {render}
+          </tbody>
+          <tfoot></tfoot>
+        </table>
+        {props.routes?
            <Switch>
-           {this.props.routes.map (route => {
+           {props.routes.map (route => {
                     return <RouteWithSubRoutes key={route.path} {...route} />
                 })}
            </Switch>:null}
-
-               </React.Fragment>)
-               }
-   else{
-       return(
-           <Fragment>
-               {t}
-           </Fragment>
-       )
-   }
+      </div>
+    </>
+  )
 }
-}
-
-export default Assets;
