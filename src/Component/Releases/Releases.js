11c4b89175591acd8b2884b6efe04461265ee02d
saideep-ss <57660981+saideep-ss@users.noreply.github.com>
2019-11-26 16:07:19 +0530

    Update Releases.js

--- src/Component/Releases/Releases.js
+++ src/Component/Releases/Releases.js
@@ -1,55 +1,36 @@
-/* eslint-disable no-undef */
-import React, {Fragment} from 'react';
-import {Switch,Redirect} from 'react-router-dom';
+import React, {useState,useEffect} from 'react';
+import {Switch,useParams} from 'react-router-dom';
 import RouteWithSubRoutes from '../RouteWithSubRoutes/RouteWithSubRoutes';
 import LoadingComponent from '../LoadingComponent/LoadingComponent';
+import Axios from 'axios';
 
-class Releases extends React.Component{
-    constructor(props){
-        super(props)
-        this.state={
-            isauth:false,
-            render: <LoadingComponent/>
+export default function Releases(props){
+    const parameter=useParams();
+  const [render,setRender]=useState(<tr><LoadingComponent/></tr>);
+  useEffect(()=>{
+    const stackKey=parameter.id;
+    const getReleases= async()=>{
+      let render=[];
+      let result =await Axios.get('/v3/releases',
+      {
+        headers:{
+          "api_key":stackKey
         }
-        this.logoutHandler=this.logoutHandler.bind(this);
-    }
-    async logoutHandler () {
-        let result = await axios.delete ('/user-session');
-        console.log (result);
-        this.setState ({isauth: false, render: <Redirect to="/login" />});
-      }
-    async componentDidMount(){
-       let r=[]
-       let api_key=window.location.pathname.split('/')[2];
-        const callApi=async ()=>{
-            let result=await axios.get('/v3/releases',
-                                    {headers:{
-                                        "api_key":api_key
-                                    }});
-            result.data.releases.forEach((e,i)=>{
-                // r.push(<tr key={i}>
-                //     <td></td>
-                //     <td></td>
-                //     <td></td>
-                // </tr>)
-            })
-            this.setState({isauth:true,r:r})
-        }
-        try{
-        callApi()
-        }catch{
-            console.log("Error at api")
-        }
-       }
+      });
+    result.data.releases.forEach((e,i)=>{
+        // render.push(<tr key={i}></tr>)
+    })
+    setRender(render)
+  }
+  getReleases();
+  },[])
 
-   render(){
-    let t=this.state.render
-    if(this.state.isauth){
-     return(
-         <React.Fragment>
-            <div className="message-display">Releases goes here</div>
-            <br />
-               <table>
+  return(
+    <>
+      <div>
+        <div className="message-display">Rleases goes here</div>
+        <br/>
+        <table>
                    <thead>
                        <tr>
                         <td>Name</td>
@@ -60,30 +41,22 @@ class Releases extends React.Component{
                    </thead>
                    <tbody>
                        <tr>
-                           <td></td><td></td><td></td>
+                           <td></td>
+                           <td></td>
+                           <td></td>
                            <td></td>
                        </tr>
-                   {this.state.r}
+                   {render}
                    </tbody>
                    <tfoot></tfoot>
                </table>
-               {this.props.routes?
+        {props.routes?
            <Switch>
-           {this.props.routes.map (route => {
+           {props.routes.map (route => {
                     return <RouteWithSubRoutes key={route.path} {...route} />
                 })}
            </Switch>:null}
-               </React.Fragment>
-        )
+      </div>
+    </>
+  )
 }
-    else{
-        return(
-            <Fragment>
-                {t}
-            </Fragment>
-        )
-    }
- }
-}
-
-export default Releases;
