cc1e12005ab14938e6b0f9865ce0d9117293250e
saideep-ss <57660981+saideep-ss@users.noreply.github.com>
2019-11-26 16:03:03 +0530

    Update GeneralComponent.js

--- src/Component/GeneralComponent/GeneralComponent.js
+++ src/Component/GeneralComponent/GeneralComponent.js
@@ -1,71 +1,77 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-/* eslint-disable no-undef */
-import React, {Fragment} from 'react';
+import React from 'react';
 import RouteWithSubRoutes from '../RouteWithSubRoutes/RouteWithSubRoutes';
-import {Switch,Link,Redirect,} from 'react-router-dom';
-import LoadingComponent from '../LoadingComponent/LoadingComponent';
+import {Switch, Link, useHistory, useParams} from 'react-router-dom';
+import Axios from 'axios';
 
-class GeneralComponent extends React.Component {
-  constructor (props) {
-    super (props);
-    this.state = {
-      isauth: true,
-      render:<LoadingComponent/>,
-    };
-    this.logoutHandler = this.logoutHandler.bind (this);
-  }
-
-  async logoutHandler () {
-    let result = await axios.delete ('/user-session');
-    console.log (result);
-    this.setState ({isauth: false, render: <Redirect to="/login" />});
-  }
-  render () {
-    let Url=window.location.pathname
-    let t = this.state.render;
-    if (this.state.isauth) {
-      let stackId = Url.split ('/')[2];
-        return (
-          <div>
-            <nav>
-              <ul>
-                <li className="nav">
-                  <Link to={`/stack/${stackId}/dashboard`}>Dashboard</Link>
-                </li>
-                <li className="nav">
-                  <Link to={`/stack/${stackId}/content-types`}>Content</Link>
-                </li>
-                <li className="nav">
-                  <Link to={`/stack/${stackId}/assets`}>Assets</Link>
-                </li>
-                <li className="nav">
-                  <Link to={`/stack/${stackId}/publish-queue`}>
-                    Publish Queue
-                  </Link>
-                </li>
-                <li className="nav">
-                  <Link to={`/stack/${stackId}/releases/list`}>Releases</Link>
-                </li>
-                <li className="nav" onClick={this.logoutHandler}>
-                  <a>Logout</a>
-                </li>
-              </ul>
-            </nav>
-            <Switch>
-            {this.props.routes.map (route => {
-            return <RouteWithSubRoutes key={route.path} {...route} />
+export default function GeneralComponent (props) {
+  const parameter = useParams ();
+  const history = useHistory ();
+  const stackKey = parameter.id;
+  const logoutHandler = async () => {
+    await Axios.delete ('/user-session');
+    history.push ('/login');
+  };
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li className="nav">
+            <Link
+              to={{
+                pathname: `/stack/${stackKey}/dashboard`,
+              }}
+            >
+              Dashboard
+            </Link>
+          </li>
+          <li className="nav">
+            <Link
+              to={{
+                pathname: `/stack/${stackKey}/content-types`,
+              }}
+            >
+              Content
+            </Link>
+          </li>
+          <li className="nav">
+            <Link
+              to={{
+                pathname: `/stack/${stackKey}/assets`,
+              }}
+            >
+              Assets
+            </Link>
+          </li>
+          <li className="nav">
+            <Link
+              to={{
+                pathname: `/stack/${stackKey}/publish-queue`,
+              }}
+            >
+              Publish Queue
+            </Link>
+          </li>
+          <li className="nav">
+            <Link
+              to={{
+                pathname: `/stack/${stackKey}/releases/list`,
+              }}
+            >
+              Releases
+            </Link>
+          </li>
+          <li className="nav" onClick={logoutHandler}>
+            <a>Logout</a>
+          </li>
+        </ul>
+      </nav>
+      {props.routes
+        ? <Switch>
+            {props.routes.map (route => {
+              return <RouteWithSubRoutes key={route.path} {...route} />;
             })}
           </Switch>
-          </div>
-        );
-    } else {
-      return (
-        <Fragment>
-          {t}
-        </Fragment>
-      );
-    }
-  }
+        : null}
+    </div>
+  );
 }
-
-export default GeneralComponent;
