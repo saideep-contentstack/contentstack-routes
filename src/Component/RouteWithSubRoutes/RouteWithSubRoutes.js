37c46261f67cda3a9a57ec882332960450802160
saideep-ss <57660981+saideep-ss@users.noreply.github.com>
2019-11-26 16:08:50 +0530

    Update RouteWithSubRoutes.js

--- src/Component/RouteWithSubRoutes/RouteWithSubRoutes.js
+++ src/Component/RouteWithSubRoutes/RouteWithSubRoutes.js
@@ -1,83 +1,18 @@
 import React from 'react';
-import {Route, Redirect} from 'react-router-dom';
-import LoadingComponent from '../LoadingComponent/LoadingComponent';
+import {BrowserRouter as Router, Switch} from 'react-router-dom';
+import RouteWithSubRoutes from '../RouteWithSubRoutes/RouteWithSubRoutes';
+import routes from '../../Routes/Routes';
 
-class RouteWithSubRoutes extends React.Component {
- state={
-   rerender:false
- }
- componentDidMount(){
-  if (this.props.middleware) {
-    let promiseArray=[];
-    this.props.middleware.forEach((e)=>{
-      let result=e();
-      promiseArray.push(result);
-    })
-    Promise.all(promiseArray)
-    .then((response)=>{
-      this.setState ({
-        rerender:true,
-        render: (
-          <Route
-            path={this.props.path}
-            render={props => {
-              return (
-                <this.props.component
-                  {...props}
-                  routes={this.props.routes}
-                />
-              );
-            }}
-          />
-        ),
-      });
-    }).catch (error => {
-      if (this.props.redirect) {
-        this.setState ({
-          rerender:true,
-          render: <Redirect to={this.props.redirect} />,
-        });
-      } else {
-        this.setState ({
-          rerender:true,
-          render: (
-            <Route
-              path={this.props.path}
-              render={props => {
-                return (
-                  <this.props.component
-                    {...props}
-                    routes={this.props.routes}
-                  />
-                );
-              }}
-            />
-          ),
-        });
-      }
-    });
-}else{
-  this.setState ({
-    rerender:true,
-    render: (
-      <Route
-        path={this.props.path}
-        render={props => {
-          return (
-            <this.props.component
-              {...props}
-              routes={this.props.routes}
-            />
-          );
-        }}
-      />
-    ),
-  });
+export default function Root(){
+  return(
+    <>
+    <Router>
+      <Switch>
+        {routes.map(route=>(
+          <RouteWithSubRoutes key={route.path} {...route}/>
+        ))}
+      </Switch>
+    </Router>
+    </>
+  )
 }
- }
-  render () {
-    let r=<LoadingComponent/>
-    return this.state.rerender?this.state.render:r
-  }
-}
-export default RouteWithSubRoutes;
