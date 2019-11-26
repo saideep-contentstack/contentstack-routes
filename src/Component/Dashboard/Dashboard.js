91a23ec52e3407dcc3abf88cec197f994cdc1b29
saideep-ss <57660981+saideep-ss@users.noreply.github.com>
2019-11-26 16:01:08 +0530

    Update Dashboard.js

--- src/Component/Dashboard/Dashboard.js
+++ src/Component/Dashboard/Dashboard.js
@@ -1,61 +1,29 @@
 /* eslint-disable no-undef */
-import React, {Fragment} from 'react';
-import {Switch,Redirect} from 'react-router-dom';
+import React, {useState,useEffect} from 'react';
+import {Switch,useLocation} from 'react-router-dom';
 import RouteWithSubRoutes from '../RouteWithSubRoutes/RouteWithSubRoutes';
-import LoadingComponent from '../LoadingComponent/LoadingComponent';
- export default class Dashboard extends React.Component{
-     constructor(props){
-        super(props)
-        this.state={
-            isauth:false,
-            render:<LoadingComponent/>
-        }
-        this.logoutHandler=this.logoutHandler.bind(this);
-     }
-     async logoutHandler () {
-        let result = await axios.delete ('/user-session');
-        console.log (result);
-        this.setState ({isauth: false, render: <Redirect to="/login" />});
-      }
-     async componentDidMount(){
-        try{
-            let r=[]
-            const callApi=async ()=>{
-                    r="Welcome to contentstack dashboard"
-                this.setState({r:r})
-            }
-            try{
-                callApi()
-            }catch{
-                console.log("Error at api")
-            }
-               this.setState({isauth:true})
-        }catch{
-            console.log("from catch");
-        }
-    }
 
- render(){
-    let t=this.state.render
-    if(this.state.isauth){
-        return(
-            <React.Fragment>
-                <div className="message-display">{this.state.r}</div>
-            {this.props.routes?
+export default function Dashboard(props){
+  // const location=useLocation();
+  // const [render,setRender]=useState(<tr><LoadingComponent/></tr>);
+  useEffect(()=>{
+    // const stackKey=location.state.stackKey;
+    const getDashboard= async()=>{}
+    getDashboard();
+  },[])
+
+  return(
+    <>
+      <div>
+        <div className="message-display">Welcome to contentstack dashboard</div>
+        <br/>
+       {props.routes?
            <Switch>
-           {this.props.routes.map (route => {
+           {props.routes.map (route => {
                     return <RouteWithSubRoutes key={route.path} {...route} />
                 })}
            </Switch>:null}
-               </React.Fragment>
-            )
-    }
-    else{
-        return(
-            <Fragment>
-                {t}
-            </Fragment>
-        )
-    }
+      </div>
+    </>
+  )
 }
- }
