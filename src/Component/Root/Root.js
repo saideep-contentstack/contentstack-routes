a55f54f5e3125406d8418960d81b1af9e868c220
saideep-ss <57660981+saideep-ss@users.noreply.github.com>
2019-11-26 16:08:19 +0530

    Update Root.js

--- src/Component/Root/Root.js
+++ src/Component/Root/Root.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch} from 'react-router-dom';
 import RouteWithSubRoutes from '../RouteWithSubRoutes/RouteWithSubRoutes';
-import routes from '../Routes';
+import routes from '../../Routes/Routes';
 
-export default class Root extends React.Component {
-  render () {
-    return (
-      <React.Fragment>
-        <Router>
-          <Switch>
-            {routes.map (route => (
-              <RouteWithSubRoutes key={route.path} {...route} />
-            ))}
-          </Switch>
-        </Router>
-      </React.Fragment>
-    );
-  }
+export default function Root(){
+  return(
+    <>
+    <Router>
+      <Switch>
+        {routes.map(route=>(
+          <RouteWithSubRoutes key={route.path} {...route}/>
+        ))}
+      </Switch>
+    </Router>
+    </>
+  )
 }
