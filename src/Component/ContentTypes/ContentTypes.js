49dcdeffd0e3b41f88ed19e8015fbfb4b8296088
saideep-ss <57660981+saideep-ss@users.noreply.github.com>
2019-11-26 16:00:43 +0530

    Update ContentTypes.js

--- src/Component/ContentTypes/ContentTypes.js
+++ src/Component/ContentTypes/ContentTypes.js
@@ -1,91 +1,72 @@
 /* eslint-disable no-undef */
-import React, {Fragment} from 'react';
-import {Link, Switch,Redirect} from 'react-router-dom';
+import React, {useState,useEffect} from 'react';
+import {Link, Switch,useParams} from 'react-router-dom';
 import RouteWithSubRoutes from '../RouteWithSubRoutes/RouteWithSubRoutes';
 import LoadingComponent from '../LoadingComponent/LoadingComponent';
+import Axios from 'axios';
 
-class ContentType extends React.Component{
-    constructor(props){
-        super(props)
-        this.state={
-            r:<LoadingComponent/>,
-            isauth:false,
-            render:<LoadingComponent/>
+export default function ContentTypes(props){
+  const [render,setRender]=useState(<tr><LoadingComponent/></tr>);
+  const paramter=useParams();
+  useEffect(()=>{
+    const stackKey=paramter.id;
+    const getAssets= async()=>{
+      let render=[];
+      let result =await Axios.get('/v3/content_types',
+      {
+        headers:{
+          "api_key":stackKey
         }
-           this.logoutHandler=this.logoutHandler.bind(this);
-    }
-    async logoutHandler () {
-        let result = await axios.delete ('/user-session');
-        this.setState ({isauth: false, render: <Redirect to="/login" />});
-      }
-    async componentDidMount(){
-       try{
-       let r=[]
-        let url=window.location.pathname;
-        let stackIdArray=url.split('/')
-        let stackId=stackIdArray[2];
-        const callApi=async ()=>{
-            let result=await axios.get('/v3/content_types',
-                                    {headers:{
-                                        "api_key":stackId
-                                    }});
-            result.data.content_types.forEach((e,i)=>{
-                r.push(<tr key={i}>
-                    <td><Link to={`/stack/${stackId}/content-type/${e.uid}/en-us/entries`}>{e.title}</Link></td>
-                    <td>{e.description}</td>
-                    <td>{e.updated_at}</td>
-                </tr>)
-            })
-            this.setState({isauth:true,r:r})
-        }
-        try{
-        callApi()
-        }catch{
-        }
-       }catch{
-       }
-   }
+      });
+    result.data.content_types.forEach((e,i)=>{
+      render.push(
+        <tr key={i}>
+          <td><Link to={{
+            pathname:`/stack/${stackKey}/content-type/${e.uid}/en-us/entries`,
+            }}>
+            {e.title}
+            </Link>
+          </td>
+          <td>{e.description}</td>
+          <td>{e.updated_at}</td>
+      </tr>
+      )
+    })
+    setRender(render)
+  }
+  getAssets();
+  },[])
 
-render(){
-    console.log(window.location.pathname,"from contenttypes")
-   let t=this.state.render
-   let authenticated=this.state.isauth;
-   if(authenticated){
-       return(
-           <React.Fragment>
-           <div>
-               <div className="message-display">Content-types goes here....</div>
-               <br />
-               <table>
-                   <thead>
-                       <tr>
-                        <td>Title</td>
-                        <td>Description</td>
-                        <td>Updated at</td>
-                       </tr>
-                   </thead>
-                   <tbody>
-                   {this.state.r}
-                   </tbody>
-                   <tfoot></tfoot>
-               </table>
-           </div>
-           {this.props.routes?
+  return(
+    <>
+      <div>
+        <div className="message-display">Content type goes here</div>
+        <br/>
+        <table>
+          <thead>
+            <tr>
+              <td>Title</td>
+              <td>Description</td>
+              <td>Updated at</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+              {render}
+          </tbody>
+          <tfoot></tfoot>
+        </table>
+        {props.routes?
            <Switch>
-           {this.props.routes.map (route => {
+           {props.routes.map (route => {
                     return <RouteWithSubRoutes key={route.path} {...route} />
                 })}
            </Switch>:null}
-           </React.Fragment>)
-   }
-   else{
-       return(
-           <Fragment>
-               {t}
-           </Fragment>
-       )
-   }
+      </div>
+    </>
+  )
 }
-}
-
-export default ContentType;
