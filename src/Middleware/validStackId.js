f00bfd6a7e5fc594cc2496521fbbe3606670d800
saideep-ss <57660981+saideep-ss@users.noreply.github.com>
2019-11-27 12:47:04 +0530

    Update validStackId.js

--- src/Middleware/validStackId.js
+++ src/Middleware/validStackId.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-undef */
-let validStackId=()=>{return new Promise((resolve,reject)=>{
+let validStackId=(data)=>{return new Promise((resolve,reject)=>{
+    const data="Some data";
     try{
         let url=window.location.pathname
         let stackId=url.split('/')[2]
@@ -7,7 +8,7 @@ let validStackId=()=>{return new Promise((resolve,reject)=>{
         .then((response)=>{
             response.data.stacks.forEach((e)=>{
                 if(e.api_key===stackId){
-                    resolve(true);
+                    resolve({valid:true,data:data});
                 }
             })
             reject(false);
