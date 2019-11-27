0b28931d4ac33154e3f0df911e6f2fce6ad78815
saideep-ss <57660981+saideep-ss@users.noreply.github.com>
2019-11-27 12:45:47 +0530

    Update alreadyLoggedIn.js

--- src/Middleware/alreadyLoggedIn.js
+++ src/Middleware/alreadyLoggedIn.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-undef */
-const alreadyLoggedIn =()=>{return new Promise((resolve,reject)=>{
-    console.log("from already logged in");
+const alreadyLoggedIn =(data)=>{return new Promise((resolve,reject)=>{
+    const data="Some data"
     try{
         axios.get(`/user`)
         .then((response)=>{
-            if(response.status===200) resolve(true)
+            if(response.status===200) resolve({valid:true,data:data})
             else reject(false)
         }).catch((error)=>{
             reject(false)
